d388d00214aad76e3726fe17144bd06710fce870
Rahul K Nair <mrmaverick357@gmail.com>
2026-03-30 23:26:39 +0530

    Update MySQL connection to use environment variables
    Refactored database connection to use environment variables for configuration.

--- database.js
+++ database.js
@@ -1,18 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Artboard1939#",
-  database: "hospital_network"
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("MySQL connection error:", err);
-  } else {
-    console.log("MySQL connected successfully");
-  }
-});
-
-module.exports = db;
+module.exports = connection;
